64d4f77bec480e1ee2758b8bb2afb26df2a42675
Method Mkoma <gershommethod02@gmail.com>
2024-05-03 22:38:26 +0300

    Fixing

--- main.c
+++ main.c
@@ -10,26 +10,27 @@
 
 char **tokenizeInput(char *_cmd, int *_l)
 {
-    int _i = 0;
-    char **_par = NULL;
-    char *_tkn = NULL;
+	int _i = 0;
+	char **_par = NULL;
+	char *_tkn = NULL;
 
-    *_l = wordsno(_cmd, ' ');
-    _par = (char **)malloc(sizeof(char *) * (*_l + 1));
-    if (_par == NULL)
-    {
-        return (NULL);
-    }
+	*_l = wordsno(_cmd, ' ');
+	_par = (char **)malloc(sizeof(char *) * (*_l + 1));
+	if (_par == NULL)
+	{
+		free(_par);
+		return (NULL);
+	}
 
-    _tkn = _strtok(_cmd, DELIM);
-    while (_tkn != NULL)
-    {
-        _par[_i++] = _strdup(_tkn);
-        _tkn = _strtok(NULL, DELIM);
-    }
-    _par[_i] = NULL;
+	_tkn = _strtok(_cmd, DELIM);
+	while (_tkn != NULL)
+	{
+		_par[_i++] = _strdup(_tkn);
+		_tkn = _strtok(NULL, DELIM);
+	}
+	_par[_i] = NULL;
 
-    return (_par);
+	return (_par);
 }
 
 /**
@@ -42,19 +43,20 @@ char **tokenizeInput(char *_cmd, int *_l)
 
 void execute(char **_par, char *command, char **env, char *_cmd)
 {
-    if (_par == NULL)
-    {
-        free(_cmd);
-        return;
-    }
+	if (_par == NULL)
+	{
+		free(_cmd);
+		return;
+	}
 
-    if (execve(command, _par, env) == -1)
-    {
-        perror("execve");
-        free(_cmd);
-        freeTokenizedInput(_par, 0); 
-        exit(127);
-    }
+	if (execve(command, _par, env) == -1)
+	{
+		perror("execve");
+		free(_par);
+		exit(127);
+	}
+
+	free(_par);
 }
 
 /**
@@ -62,36 +64,36 @@ void execute(char **_par, char *command, char **env, char *_cmd)
  * @_par: Array of strings containing the command and its arguments
  * @env: Array of strings containing the environment variables
  * @_cmd: The original input command string
+ * @_l: integer
  */
 
-void searchAndExecute(char **_par, char **env, char *_cmd)
+void searchAndExecute(char **_par, char **env, char *_cmd, int _l)
 {
-    char *path = getenv("PATH");
-    char *token = _strtok(path, ":");
-    char cmd_path[1024];
-    int found = 0;
+	char *path = getenv("PATH");
+	char *token = _strtok(path, ":");
+	char cmd_path[1024];
+	int found = 0;
 
-    while (token != NULL)
-    {
-        _strcpy(cmd_path, token);
-        _strcat(cmd_path, "/");
-        _strcat(cmd_path, _par[0]);
-        if (access(cmd_path, X_OK) == 0)
-        {
-            _par[0] = _strdup(cmd_path);
-            execute(_par, _par[0], env, _cmd);
-            found = 1;
-            break;
-        }
-        token = _strtok(NULL, ":");
-    }
-    if (!found)
-    {
-        customErrorPrint(_par[0]);
-        free(_cmd);
-        freeTokenizedInput(_par, 0); 
-        exit(127);
-    }
+	while (token != NULL)
+	{
+		_strcpy(cmd_path, token);
+		_strcat(cmd_path, "/");
+		_strcat(cmd_path, _par[0]);
+		if (access(cmd_path, X_OK) == 0)
+		{
+			_par[0] = _strdup(cmd_path);
+			execute(_par, _par[0], env, _cmd);
+			found = 1;
+			break;
+		}
+		token = _strtok(NULL, ":");
+	}
+	if (!found)
+	{
+		freeTokenizedInput(_par, _l);
+		customErrorPrint(_par[0]);
+		exit(127);
+	}
 }
 
 /**
@@ -102,51 +104,46 @@ void searchAndExecute(char **_par, char **env, char *_cmd)
 
 void handleCommand(char *_cmd, char **env)
 {
-    int _l, status;
-    char **_par = tokenizeInput(_cmd, &_l);
-    pid_t _child;
+	int  _l, status;
+	char **_par = tokenizeInput(_cmd, &_l);
+	pid_t _child;
 
-    if (_par == NULL)
-    {
-        free(_cmd);
-        return;
-    }
+	if (_par == NULL)
+	{
+		free(_cmd);
+		return;
+	}
 
-    if (_par[0] != NULL)
-    {
-        if (_strcmp(_par[0], "exit") == 0)
-            exitShell(_cmd, _par);
-        else if (_strcmp(_par[0], "env") == 0)
-            printEnvironment(env);
-        else if (_strcmp(_par[0], "cd") == 0)
-            changeDirectory(_par);
-        _child = fork();
-        if (_child == -1)
-        {
-            perror("fork");
-            freeTokenizedInput(_par, _l); 
-            free(_cmd);
-            exit(127);
-        }
-        if (_child == 0)
-        {
-            if (_strchr(_par[0], '/') != NULL)
-            {
-                execute(_par, _par[0], env, _cmd);
-            }
-            else
-            {
-                searchAndExecute(_par, env, _cmd);
-            }
-            exit(127);
-        }
-        else
-        {
-            waitpid(_child, &status, 0);
-            freeTokenizedInput(_par, _l); 
-            free(_cmd);
-        }
-    }
+	if (_par[0] != NULL)
+	{
+		if (_strcmp(_par[0], "exit") == 0)
+			exitShell(_cmd, _par);
+		else if (_strcmp(_par[0], "env") == 0)
+			printEnvironment(env);
+		else if (_strcmp(_par[0], "cd") == 0)
+			changeDirectory(_par);
+		_child = fork();
+		if (_child == -1)
+		{
+			perror("fork");
+			exit(127);
+		}
+		if (_child == 0)
+		{
+			if (_strchr(_par[0], '/') != NULL)
+			{
+				execute(_par, _par[0], env, _cmd);
+			}
+			else
+			{
+				searchAndExecute(_par, env, _cmd, _l);
+			}
+			exit(127);
+		}
+		else
+			waitpid(_child, &status, 0);
+	}
+	freeTokenizedInput(_par, _l);
 }
 
 /**
@@ -159,26 +156,29 @@ void handleCommand(char *_cmd, char **env)
 
 int main(int argc, char *argv[], char **env)
 {
-    char *_cmd = NULL;
-    size_t _len = 0;
-    ssize_t _r;
+	char *_cmd = NULL;
+	size_t _len = 0;
+	ssize_t _r;
 
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 
-    while (1)
-    {
-        dollar();
-        _r = getline(&_cmd, &_len, stdin);
-        if (_r == -1)
-        {
-            free(_cmd);
-            break;
-        }
-        _cmd[_strlen(_cmd) - 1] = '\0';
-        handleComments(_cmd);
-        handleCommand(_cmd, env);
-    }
+	while (1)
+	{
+		dollar();
+		_r = getline(&_cmd, &_len, stdin);
+		if (_r == -1)
+		{
+			break;
+		}
+		_cmd[_strlen(_cmd) - 1] = '\0';
+		handleComments(_cmd);
+		handleCommand(_cmd, env);
+		free(_cmd);
+		_cmd = NULL;
+	}
 
-    return (0);
+
+	free(_cmd);
+	return (0);
 }
